110f920ab277fc6111b4c4e2452814621c991a81
Megan Day <meganeday7@gmail.com>
2021-04-27 11:21:25 -0500

    commit tree b06e59689bea3c6aba398bde4b7cfbe90361ca52

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+
+
+
+function upperCaseCustomerName(customerName) {
+    return customerName.toUpperCase();
+};
+
+upperCaseCustomerName(bob);
+
+
+function setBestCustomer() {
+    const bestCustomer = "not bob";
+    return bestCustomer;
+};
